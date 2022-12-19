6e217b14af07be08bb6b30bfde2472cde4e43556
kaizhang <kaizhang@yahoo.com>
2022-12-19 21:55:17 +0000

    market api trimmed, dashboard does more

--- macro_widgets.py
+++ macro_widgets.py
@@ -4,17 +4,34 @@ import pandas_datareader.data as web
 import plotly.graph_objects as go
 import plotly.express as px
 import datetime as dt
+import requests
+from getEODprice import chunks
 
 
 def get_vix_from_yahoo(start_date):
-    df_vix = web.DataReader('^VIX', 'yahoo', start=start_date)
+    # df_vix = web.DataReader('^VIX', 'yahoo', start=start_date)
+    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+    epoch_start = int(dt.datetime.strptime(start_date, '%Y-%m-%d').timestamp())
+    base_url = f"https://query1.finance.yahoo.com/v8/finance/chart/^VIX?period1={epoch_start}&period2=9999999999&interval=1d&events=history"
+    try:
+        r = requests.get(base_url, headers=header)
+        p = r.json()
+        dates = p['chart']['result'][0]['timestamp']
+        ohlc_json = p['chart']['result'][0]['indicators']['quote'][0]
+        df_vix = pd.DataFrame.from_dict(ohlc_json)
+        df_vix['Date'] = [dt.datetime.fromtimestamp(x).date() for x in dates]
+        df_vix.index = df_vix['Date']
+    except Exception as e:
+        print(e)
+        return None
+
     fig = go.Figure(data=[go.Candlestick(x=df_vix.index,
-            open=df_vix['Open'],
-            high=df_vix['High'],
-            low=df_vix['Low'],
-            close=df_vix['Close'])])
+            open=df_vix['open'],
+            high=df_vix['high'],
+            low=df_vix['low'],
+            close=df_vix['close'])])
     fig.update_layout(yaxis_title='VIX', xaxis_title='Date')
-    df_vix.index = df_vix.index.strftime('%Y-%m-%d')
+    # df_vix.index = df_vix.index.strftime('%Y-%m-%d')
     return fig, df_vix.tail(1)
 
 def real_gdp_pct_change(start_date):
@@ -109,6 +126,27 @@ def sp500_winner_loser_treemap():
         color_continuous_scale='burg')
         #color_discrete_map={'Day Pctage Change': px.colors.sequential.speed})
     return winner_fig, loser_fig
+
+def sector_etf_map(df: pd.DataFrame):
+    last_column = df.columns[-1]
+    #df['display_no'] = df[last_column].apply(lambda x: "+" + str(x) if x > 0 else str(x))
+    df['display_no'] = df[last_column].apply(lambda x: "+" + str(round(x, 2)) if x > 0 else str(round(x, 2)))
+    df['display'] = df['Sector'] + " (" + df['display_no'] + "%)"
+    df['hovertext'] = df['ETF'] + "  " + df['Last Price'].astype(str)
+    df.sort_values(by=last_column, inplace=True, ascending=False)
+    tiles = list(chunks(df['display'], 3))
+    tile_index  = list(chunks(df[last_column], 3))
+    hovertexts = list(chunks(df['hovertext'], 3))
+
+    # fig = px.imshow(tile_index, labels=dict(x="Sector", color="PCT Change"), color_continuous_scale=px.colors.sequential.speed, aspect='auto')
+    # fig.update_layout(title_text=last_column)
+    # fig.update_traces(text=tiles, texttemplate='%{text}')
+    
+    fig = go.Figure(data=go.Heatmap(z=tile_index[::-1], colorscale=px.colors.sequential.speed, texttemplate="%{text}",
+                                    text=tiles[::-1], textfont={'size': 12}, hovertemplate='%{hovertext}', hovertext=hovertexts[::-1]))
+    return fig
+
+
 
 def main():
     start_date = '2022-11-01'
